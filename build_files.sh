304e8553a9ee6ee915902c74e4e7d549efbae7eb
anower77 <anowerhossain765562@gmail.com>
2024-12-28 03:45:21 +0600

    add files 3

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,11 @@
 # build_files.sh
+echo "Building the project..."
+python -m pip install --upgrade pip
 pip install -r requirements.txt
-python manage.py collectstatic --noinput 
+
+echo "Make migrations..."
+python manage.py makemigrations
+python manage.py migrate
+
+echo "Collect Static..."
+python manage.py collectstatic --noinput --clear 
