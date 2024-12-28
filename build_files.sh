5bbb6b3831e4c039c82340d38f12246a0085bb67
anower77 <anowerhossain765562@gmail.com>
2024-12-28 11:19:00 +0600

    deploy issue

--- build_files.sh
+++
@@ -1,9 +0,0 @@
-# build_files.sh
-echo "Installing dependencies..."
-pip install -r requirements.txt
-
-echo "Collecting static files..."
-python manage.py collectstatic --noinput
-
-echo "Running migrations..."
-python manage.py migrate 
